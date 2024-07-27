b1c32417153d71c3c3a5476108dd165effe83465
sree-r-one <sreeraj_ec@hotmail.com>
2024-07-27 22:45:50 +0800

    commit of the day

--- academind-section19-redux/src/components/Counter.js
+++ academind-section19-redux/src/components/Counter.js
@@ -1,5 +1,6 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
@@ -7,19 +8,19 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", value: 5 });
+    dispatch(counterActions.increase(10));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   return (
--- academind-section19-redux/src/store/index.js
+++ academind-section19-redux/src/store/index.js
@@ -1,43 +1,30 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({});
-
-// Create reducer function
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment":
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
-
-    case "increase":
-      return {
-        ...state,
-        counter: state.counter + action.value,
-      };
-
-    case "decrement":
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
-
-    case "toggle":
-      return {
-        ...state,
-        showCounter: !state.showCounter,
-      };
-
-    default:
-      return state;
-  }
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter++;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 // Create a store
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
+export const counterActions = counterSlice.actions;
 export default store;
